36e6ba0c217183d345cb0be7c4c9276e74542a53
Thomas Watson Steen <w@tson.dk>
2015-09-12 19:25:38 +0200

    Use rtsp-stream module

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict'
 
 var net = require('net')
-var pump = require('pump')
-var debug = require('debug')('rtsp-server')
+var rtsp = require('rtsp-stream')
+var pkg = require('./package')
+var debug = require('debug')(pkg.name)
 
 exports.CLIENT_METHODS = require('./lib/client-methods')
 exports.SERVER_METHODS = require('./lib/server-methods')
-exports.STATUS_CODES = require('./lib/status-codes')
-
-var IncomingMessage = exports.IncomingMessage = require('./lib/incoming-message')
-var ServerResponse = exports.ServerResponse = require('./lib/server-response')
+exports.STATUS_CODES = rtsp.STATUS_CODES
+exports.IncomingMessage = rtsp.Request
+exports.ServerResponse = rtsp.Response
 
 exports.createServer = function (onRequest) {
   var server = net.createServer()
@@ -19,24 +19,39 @@ exports.createServer = function (onRequest) {
   server.on('connection', function (socket) {
     debug('new socket connection')
 
-    var req = new IncomingMessage(socket)
+    socket.on('error', function (err) {
+      debug('error on socket connection', err.message)
+      server.emit('clientError', err, socket)
+    })
 
-    req.on('request', function () {
-      debug('%s request (CSeq: %s)', req.method, req.headers['cseq'])
-      var res = new ServerResponse(socket)
+    socket.on('end', function () {
+      debug('socket connection ended')
+    })
+
+    socket.on('close', function () {
+      debug('socket connection closed')
+    })
+
+    var decoder = new rtsp.Decoder()
+    var encoder = new rtsp.Encoder()
+
+    decoder.on('request', function (req) {
+      debug('%s %s', req.method, req.url, req.headers)
+
+      var res = encoder.response()
       res.setHeader('CSeq', req.headers['cseq'])
       res.setHeader('Date', new Date().toGMTString())
+
       server.emit('request', req, res)
     })
 
-    pump(socket, req, function (err) {
-      if (err) {
-        debug('socket connection ended with error:', err.message)
-        server.emit('error', err)
-      } else {
-        debug('socket connection ended')
-      }
+    decoder.on('error', function (err) {
+      server.emit('error', err)
+      socket.destroy()
     })
+
+    socket.pipe(decoder)
+    encoder.pipe(socket)
   })
 
   return server
--- test.js
+++ test.js
@@ -4,73 +4,57 @@ var net = require('net')
 var test = require('tape')
 var rtsp = require('./')
 
-test('no response body', function (t) {
-  t.plan(5)
+test('server events', function (t) {
+  t.plan(3)
 
-  var server = rtsp.createServer(function (req, res) {
-    t.equal(req.method, 'OPTIONS')
-    t.equal(req.url, '*')
-    t.equal(req.rtspVersion, '1.0')
-    t.deepEqual(req.headers, { 'cseq': '42', 'foo': 'bar' })
-    res.setHeader('Bar', 'baz')
-    res.end()
+  var server = rtsp.createServer()
+
+  server.on('listening', connect)
+  server.on('connection', function (c) {
+    t.ok(true)
+    c.unref()
   })
+  server.on('request', function (req, res) {
+    t.ok(req instanceof rtsp.IncomingMessage)
+    t.ok(res instanceof rtsp.ServerResponse)
+    server.unref()
+  })
+  server.listen()
 
-  server.listen(function () {
+  function connect () {
     var port = server.address().port
-    var buffers = []
-
     var client = net.connect(port, function () {
-      client.write('OPTIONS * RTSP/1.0\r\n')
-      client.write('CSeq: 42\r\n')
-      client.write('Foo: bar\r\n')
-      client.write('\r\n')
+      client.write('OPTIONS * RTSP/1.0\r\n\r\n')
     })
-
-    client.on('data', buffers.push.bind(buffers))
-
-    client.on('end', function () {
-      var data = Buffer.concat(buffers).toString()
-      t.equal(data, 'RTSP/1.0 200 OK\r\nCSeq: 42\r\nBar: baz\r\n\r\n')
-    })
-  })
-
-  server.unref()
+    client.unref()
+  }
 })
 
-test('with response body', function (t) {
-  t.plan(5)
+test('request events', function (t) {
+  t.plan(3)
 
-  var date
   var server = rtsp.createServer(function (req, res) {
-    t.equal(req.method, 'OPTIONS')
-    t.equal(req.url, '*')
-    t.equal(req.rtspVersion, '1.0')
-    t.deepEqual(req.headers, { 'cseq': '42', 'foo': 'bar' })
-    res.setHeader('Bar', 'baz')
-    res.write('Hello World!')
-    res.end()
-    date = new Date().toGMTString()
+    req.on('readable', function () {
+      t.ok(true)
+    })
+    req.on('end', function () {
+      t.ok(true)
+      server.unref()
+    })
+    req.on('data', function (chunk) {
+      t.equal(chunk.toString(), 'foobar')
+    })
+  })
+
+  server.on('connection', function (c) {
+    c.unref()
   })
 
   server.listen(function () {
     var port = server.address().port
-    var buffers = []
-
     var client = net.connect(port, function () {
-      client.write('OPTIONS * RTSP/1.0\r\n')
-      client.write('CSeq: 42\r\n')
-      client.write('Foo: bar\r\n')
-      client.write('\r\n')
+      client.write('OPTIONS * RTSP/1.0\r\nContent-Length: 6\r\n\r\nfoobar')
     })
-
-    client.on('data', buffers.push.bind(buffers))
-
-    client.on('end', function () {
-      var data = Buffer.concat(buffers).toString()
-      t.equal(data, 'RTSP/1.0 200 OK\r\nCSeq: 42\r\nDate: ' + date + '\r\nBar: baz\r\n\r\nHello World!')
-    })
+    client.unref()
   })
-
-  server.unref()
 })
